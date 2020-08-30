50cc0033c2c37ba4d458459a4cecb6a4370f058c
Saeed Aghdam <s.mohammadi@mabnadp.com>
2020-08-30 21:15:09 +0430

    Added Semaphore's unit testing to test project

---
+++ DynaLock.Test/Semaphore.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DynaLock.Test
+{
+    public class Semaphore
+    {
+        private long _threadsInMethodCount = 0;
+        private const int TotalThreadsPermitted = 4;
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Func<bool> CheckSemaphoreBehavior = () =>
+            {
+                if (Interlocked.Read(ref _threadsInMethodCount) >= TotalThreadsPermitted)
+                {
+                    return false;
+                }
+                else
+                {
+                    Interlocked.Increment(ref _threadsInMethodCount);
+                    Thread.Sleep(10000);
+                    Interlocked.Decrement(ref _threadsInMethodCount);
+                    return true;
+                }
+            };
+
+            bool assertResult = true;
+            for (int i = 0; i < 10; i++)
+            {
+                List<Task<bool>> tasks = new List<Task<bool>>();
+                for (int j = 0; j < 12; j++)
+                {
+                    tasks.Add(Task<bool>.Factory.StartNew(() =>
+                    {
+                        bool result = false;
+                        // Only 4 threads could access resource at the same time
+                        using (var semaphore = new DynaLock.SemaphoreBuilder()
+                            .Name("semaphore_name")
+                            .InitialCount(TotalThreadsPermitted)
+                            .MaximumCount(TotalThreadsPermitted)
+                            .Build())
+                        {
+                            do
+                            {
+                                semaphore.WaitOne(60000);
+                                if (semaphore.IsLockOwned())
+                                {
+                                    // Semaphore is taken by thread
+                                    result = CheckSemaphoreBehavior();
+                                    break;
+                                }
+                            } while (true);
+                        }
+
+                        return result;
+                    }));
+                }
+
+                Task.WaitAll(tasks.ToArray());
+
+                if (tasks.Any(task => task.Result == false))
+                {
+                    assertResult = false;
+                    break;
+                }
+            }
+
+            if (assertResult)
+                Assert.Pass();
+            else
+                Assert.Fail();
+        }
+    }
+}
