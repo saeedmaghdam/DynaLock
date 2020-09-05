ea66dc4da6dfa0bad5c0e35149dd93937312be99
Saeed Mohammadi <s.mohammadi@mabnadp.com>
2020-09-05 17:06:18 +0430

    Added DynaLock factory

---
+++ DynaLock/Factory.cs
@@ -0,0 +1,9 @@
+﻿namespace DynaLock
+{
+    public class Factory
+    {
+        public MonitorBuilder MonitorBuilder() => new MonitorBuilder();
+        public SemaphoreBuilder SemaphoreBuilder() => new SemaphoreBuilder();
+        public AutoResetEventBuilder AutoResetEventBuilder() => new AutoResetEventBuilder();
+    }
+}
